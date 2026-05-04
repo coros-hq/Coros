5f34603944f17bd2ddc54a2a126337180ba7c925
oussamachah2020 <otaksam213@gmail.com>
2026-05-04 12:20:56 +0100

    fix the vercel config

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './src/app/app.module';
+import { AppModule } from '../src/app/app.module';
 import serverlessExpress from '@vendia/serverless-express';
 
 let server: any;
